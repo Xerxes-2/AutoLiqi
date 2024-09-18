a03d391b1b4a62fcea552d4bae698675834401ab
Xerxes-2 <dspxue@gmail.com>
2024-09-18 16:58:48 +1000

    Add lqc.bin

--- get_liqi.py
+++ get_liqi.py
@@ -11,9 +11,17 @@ def get_prefix(version):
     req = requests.get( f'https://game.maj-soul.com/1/resversion{version}.json',headers=Headers)
     return req.json()['res']['res/proto/liqi.json']['prefix']
 
+def get_lqc_prefix(version):
+    req = requests.get( f'https://game.maj-soul.com/1/resversion{version}.json',headers=Headers)
+    return req.json()['res']['res/config/lqc.lqbin']['prefix']
+
 def get_liqi(prefix):
     req = requests.get( f'https://game.maj-soul.com/1/{prefix}/res/proto/liqi.json',headers=Headers)
     return req.text
+
+def get_lqc(prefix):
+    req = requests.get( f'https://game.maj-soul.com/1/{prefix}/res/config/lqc.lqbin',headers=Headers)
+    return req.content
 
 def get_code_js(code):
     req = requests.get( f'https://game.maj-soul.com/1/{code}',headers=Headers)
@@ -22,16 +30,22 @@ def main():
     version = get_version ()
     code_js = get_code_js(version["code"])
     prefix = get_prefix(version['version'])
+    lqc_prefix = get_lqc_prefix(version['version'])
+    lqc = get_lqc(lqc_prefix)
     liqi =  get_liqi(prefix)
     with open('liqi.json','w') as f:
         f.write(liqi)
     with open('code.js','w') as f:
         f.write(code_js)
+    with open('lqc.bin','wb') as f:
+        f.write(lqc)
     env = os.getenv('GITHUB_ENV')
     with open(env, "a") as f:
         f.write(f"code-js=v{version['version']}\n")
     with open(env, "a") as f:
         f.write(f"liqi-json={prefix}")
+    with open(env, "a") as f:
+        f.write(f"lqc-bin={lqc_prefix}")
 
 if __name__ == '__main__':
     main()
